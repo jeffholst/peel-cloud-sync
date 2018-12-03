41e2abcf251cf2cf27f6972c2af61b9c6a7cb4c1
jeffholst <jeffholst@gmail.com>
2018-12-02 23:35:46 -0600

    Blast off!

---
+++ README.md
@@ -0,0 +1,12 @@
+# peel-cloud-sync
+
+Copy local files to rackspace container
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) installed and have customized variables in cloudCopy.js
+
+```sh
+npm install
+node cloudCopy.js
+```
---
+++ cloudCopy.js
@@ -0,0 +1,133 @@
+var fs = require('fs');
+const path = require('path');
+pkgcloud = require('pkgcloud');
+const md5File = require('md5-file');
+
+// Reference
+// https://github.com/pkgcloud/pkgcloud/blob/master/docs/providers/rackspace/storage.md
+
+/*
+  _   __         _      __   __      
+ | | / /__ _____(_)__ _/ /  / /__ ___
+ | |/ / _ `/ __/ / _ `/ _ \/ / -_|_-<
+ |___/\_,_/_/ /_/\_,_/_.__/_/\__/___/
+                                     
+*/
+
+const verbose = false;   // Print verbose messages
+
+// Array of all the paths to sync.  The basename of the path is the remote container name.
+const ContainerPath = [
+    "/home/jeff/repos/peel-cloud-sync/UploadFiles/JKH"
+];
+
+// Only sync files > checkDate
+const checkDate = new Date();
+// Set checkDate to 13 months prior to current date
+checkDate.setMonth(checkDate.getMonth() - 13);
+
+// Nothing below here should need to be changed
+
+// Client credentials
+var client = pkgcloud.storage.createClient({
+  provider: 'rackspace',
+  username: 'capeel',
+  apiKey: 'a8d8c3b44a1758f74aa94ce72e6d5391',
+  region: 'DFW'
+});
+
+// Options to use when retreiving remote files
+var options = {
+    limit: Infinity // Infinity = all files, otherwise use integer
+};
+
+function copyFile(cliient, myContainer, localFile, remoteFile){
+    // Copies localFile to myContainer/remoteFile
+
+    // Create a read stream for our source file
+    var source = fs.createReadStream(localFile);
+
+    // Create a writeable stream for our destination
+    var dest = client.upload({
+        container: myContainer,
+        remote: remoteFile
+    });
+
+    dest.on('error', function(err) {
+        // TODO handle err as appropriate
+    });
+
+    dest.on('success', function(file) {
+        // TODO handle successful upload case
+    });
+
+    // pipe the source to the destination
+    source.pipe(dest);
+}
+
+function verboseLog(msg){
+    // prints verbose messages when verbose=true
+    if (verbose){
+        console.log(msg);
+    }
+}
+
+// Loop through each directory we want to sync
+for (var containerLoop=0; containerLoop < ContainerPath.length; containerLoop++) {
+    const localPath = ContainerPath[containerLoop];
+    const myContainer = path.basename(localPath);
+    verboseLog(`Local path set to: ${localPath}`);
+
+    console.log(`Container: '${myContainer}'`);
+
+    // Retrieve array 'remoteFiles' of all remote files in container
+    client.getFiles(myContainer, options, function(err, remoteFiles){
+        verboseLog(`Remote file count: ${remoteFiles.length}`);
+
+        // Get array 'items' all all local files in directory
+        fs.readdir(localPath, function(err, localFiles) {
+            verboseLog(`Local file count: ${localFiles.length}`);
+
+            // Loop through all our local files to see which need to be synced
+            for (var fileLoop=0; fileLoop < localFiles.length; fileLoop++) {
+                fileName = localFiles[fileLoop];
+                fqp = path.join(localPath, fileName);
+
+                verboseLog(`File ${fileLoop + 1} of ${localFiles.length}`);
+                verboseLog(`   full path: ${fqp}`);
+                verboseLog(`   filename: ${fileName}`);
+                
+                var stats = fs.statSync(fqp);
+                
+                let localFileModDate = new Date(stats["mtime"]);
+                    
+                verboseLog(`   modified date: ${localFileModDate}`);
+
+                if ( localFileModDate > checkDate){
+                    remoteFile = remoteFiles.find(function(f){return f.name === fileName});
+
+                    if (remoteFile){
+                        verboseLog(`   file found in remote container`);
+                        const hash = md5File.sync(fqp);
+
+                        if (remoteFile.etag != hash){
+                            console.log(`   Copying changed file: ${fileName}`);
+                            copyFile(client, myContainer, fqp, fileName);
+                        }
+                        else{
+                            verboseLog(`   md5 same so skippin`);
+                        }
+                    }
+                    else{
+                        // File not in remote container so copy it over
+                        console.log(`   Copying new file: ${fileName}`);
+                        copyFile(client, myContainer, fqp, fileName);
+                    }
+                }
+                else{
+                    verboseLog(`   skipping because of date`);
+                }
+            }
+        });
+    });
+}
